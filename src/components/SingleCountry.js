f0130225cc1d5e411bb312dadb5a6e1ee970521b
Henry <ofosuansahh@gmail.com>
2023-03-13 09:42:26 +0000

    bg in back

--- src/components/SingleCountry.js
+++ src/components/SingleCountry.js
@@ -29,7 +29,7 @@ export default function SingleCountry() {
       <section className="p-8 md:py-0 max-w-7xl mx-auto">
             <Link
               to="/"
-                className="inline-block mt-8 py-2 px-6 rounded shadow transition-all duration-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-400"
+                className="inline-block mt-8 py-2 px-6 rounded shadow transition-all duration-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-400 bgchange"
               >
               &larr; Back
             </Link>
