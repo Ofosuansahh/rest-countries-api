fdbbe8ce0bdbf8cc2a10492b0b0ab4650e4e8792
Henry <ofosuansahh@gmail.com>
2023-03-30 16:26:39 +0000

    Updates

---
+++ precache-manifest.b8007a94293a58e92095f6d25b9d53e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ce10075e1bbb43a92be",
+    "url": "/rest-countries/static/css/main.de0ce1ad.chunk.css"
+  },
+  {
+    "revision": "2ce10075e1bbb43a92be",
+    "url": "/rest-countries/static/js/main.2ce10075.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/rest-countries/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "ec4da754ad60a635cc3e",
+    "url": "/rest-countries/static/js/2.ec4da754.chunk.js"
+  },
+  {
+    "revision": "514432409e3f5176f01c",
+    "url": "/rest-countries/static/js/runtime~main.51443240.js"
+  },
+  {
+    "revision": "0b7dd2d3f21733073205e40974c3e63b",
+    "url": "/rest-countries/static/media/moon-outline.0b7dd2d3.svg"
+  },
+  {
+    "revision": "91452371fea1db05dbb7c64473b82f09",
+    "url": "/rest-countries/static/media/man.91452371.png"
+  },
+  {
+    "revision": "e6a15c4900aff562c5a21694d20d75e3",
+    "url": "/rest-countries/index.html"
+  }
+];
